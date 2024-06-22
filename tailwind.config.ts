140a287f0a1faf07ee42019474e8d82c6e6f4b26
Daniel <danildeyneka@gmail.com>
2024-06-23 00:53:27 +0500

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+import daisyui from "daisyui"
+
+const config: {plugins: ReturnType<Plugin>[]; content: string[]} = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  plugins: [
+    daisyui,
+  ],
+};
+export default config;
